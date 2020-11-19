ea265c483eef4bd85eeb8a35b2ac970eed58b7db
Abakirov0 <abakirovdaniar34@gmail.com>
2020-11-19 15:58:32 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    String name;
+    int age = (20 + 45);
+        System.out.println(age = 20);
+        System.out.println(age = 45);
+    }
+}
